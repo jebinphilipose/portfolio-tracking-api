f29984fa227bdbfcc0bf0a9d25237c6f9a14d9e6
Jebin Philipose <jebinphilip24@gmail.com>
2019-09-10 21:37:29 +0530

    Setup useful Express middlewares

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const helmet = require('helmet');
+const cors = require('cors');
 const express = require('express');
+
 const app = express();
+
+// For parsing request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// For securing app by setting various HTTP headers
+app.use(helmet());
+
+// For enabling CORS
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
