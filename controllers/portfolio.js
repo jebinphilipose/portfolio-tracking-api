4cec0c87ca4bd6bda5b89c8b2d36d625bf58c20d
Jebin Philipose <jebinphilip24@gmail.com>
2019-09-14 14:16:22 +0530

    Add GET /api/portfolio

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -1,6 +1,20 @@
+const Trade = require('../models/trade');
 const Portfolio = require('../models/portfolio');
 
 module.exports = {
+  getPortfolio: async (req, res) => {
+    let securities = await Portfolio.find({ quantity: { $ne: 0 } }).select('tickerSymbol -_id');
+    securities = securities.map((doc) => doc.tickerSymbol);
+    const portfolio = await Promise.all(securities.map(async (tickerSymbol) => {
+      const trades = await Trade.find({ tickerSymbol }).select('-tickerSymbol -__v');
+      return {
+        [tickerSymbol]: {
+          trades,
+        },
+      };
+    }));
+    return res.status(200).json(portfolio);
+  },
   getHoldings: async (req, res) => {
     const holdings = await Portfolio.find({ quantity: { $ne: 0 } }).select('-_id -__v');
     return res.status(200).json(holdings);
--- routes/portfolio.js
+++ routes/portfolio.js
@@ -3,6 +3,9 @@ const controller = require('../controllers/portfolio');
 
 const router = express.Router();
 
+// Fetch portfolio
+router.get('/portfolio', controller.getPortfolio);
+
 // Fetch holdings
 router.get('/holdings', controller.getHoldings);
 
